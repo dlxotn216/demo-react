be70fd8cfb80f8f2d0154c69c72c2872fd72b271
taesu <taesu@crscube.co.kr>
2019-06-05 11:02:13 +0900

    Day 01 refactoring
    React router dom 적용
    
    Material ui library 추가

---
+++ src/LeftMenu.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import {NavLink, Route} from "react-router-dom";
+import "./LeftMenu.css";
+
+class LeftMenu extends Component {
+
+    handleRoute(e) {
+        if (e.target.pathname === window.location.pathname) {
+            e.preventDefault();
+        }
+    }
+
+    render() {
+        return (
+            <div className="left-menu">
+                <ul>
+                    <li><NavLink to="/day01" uri="/day01" onClick={this.handleRoute}
+                                 activeClassName="selected">Day01</NavLink></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+export default LeftMenu;
--- src/index.js
+++ src/index.js
@@ -1,71 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import "./index.css";
-import App from "./App";
-
+import Day01App from "./day01/Day01App";
 import * as serviceWorker from "./serviceWorker";
-import Menu from "./Menu";
-import Clock from "./Clock";
-import Form from "./Form";
+import LeftMenu from "./LeftMenu";
+
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="left-wrap">
+                    <LeftMenu />
+                </div>
+                <div className="right-wrap">
+                    <Route path="/day01" component={Day01App}/>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-class IngredientsList extends React.Component {
-    renderListItem(ingredient, i) {
-        return React.createElement('li', {key: i}, ingredient);
-    }
-
-    render() {
-        return React.createElement('ul', {className: 'ingredients'}, ['salmon', 'lemon'].map(this.renderListItem));
-    }
-}
-ReactDOM.render(<IngredientsList />, document.getElementById('root2'));
-
-const IngredientsList2 = props =>
-    React.createElement('ul', {className: 'ingredients'},
-        ['salmon', 'lemon'].map(
-            (ingredient, i) => React.createElement('li', {key: i}, ingredient)
-        ));
-
-ReactDOM.render(<h3>Test03</h3>, document.getElementById('root4'));
-ReactDOM.render(<IngredientsList2 />, document.getElementById('root3'));
-
-
-function TestApp() {
-    return (
-        <ul>
-            {
-                ['salmon', 'lemon'].map((ingredient, i) => <li key={i}>{ingredient}</li>)
-            }
-        </ul>
-    );
-}
-ReactDOM.render(<TestApp />, document.getElementById('root4'));
-
-const data = [
-    {
-        name: 'Baked salmon',
-        ingredients: [
-            {name: '연어', amount: 500, mesurement: 'gram'},
-            {name: '잣', amount: 1, mesurement: '컵'}
-        ]
-    },
-    {
-        name: '생선 타코',
-        ingredients: [
-            {name: '생선', amount: 500, mesurement: 'gram'},
-            {name: '치즈', amount: 1, mesurement: '컵'}
-        ]
-    }
-];
-
-
-
-ReactDOM.render(<Menu title="Menu" recipes={data} />, document.getElementById('react-container'));
-ReactDOM.render(<Clock />, document.getElementById('clock'));
-ReactDOM.render(<Form />, document.getElementById('form'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
