5b1a7b7c4814ca404490b220f8343fd0dc3482c3
taesu <taesu@crscube.co.kr>
2019-06-07 09:09:26 +0900

    Day04 Fetch
    https://restcountries.eu/#api-endpoints-all
    
    Country name fetch 데모
    리소스를 불러오는 동안 loading으로 표시하도록
    state 조작 로직 적용

---
+++ src/day04-fetch/Day04FetchApp.js
@@ -0,0 +1,51 @@
+/**
+ * @author Lee Tae Su
+ * @project demo-react
+ * @version 1.0
+ * @since 2019-06-07
+ */
+import React, {Component} from "react";
+import fetch from "isomorphic-fetch";
+
+class Day04FetchApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            countryNames: [],
+            loading: false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({loading: true});
+
+        /* https://restcountries.eu/#api-endpoints-all */
+        fetch('https://restcountries.eu/rest/v2/all')
+            .then(res => res.json())
+            .then(json => json.map(country => country.name))
+            .then(countryNames =>
+                this.setState({loading: false, countryNames}));
+    }
+
+    render() {
+        const {loading, countryNames} = this.state;
+
+        return (
+            (loading) ?
+                <div>loading...</div>
+                :
+                (!countryNames.length) ?
+                    <div>나라이름이 없습니다</div>
+                    :
+                    <ul>
+                        {
+                            countryNames.map((name, i) =>
+                                <li key={i}>{name}</li>
+                            )
+                        }
+                    </ul>
+        )
+    }
+}
+
+export default Day04FetchApp;
